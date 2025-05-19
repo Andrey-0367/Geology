6d5412bbf203465a2f793e7b53ea4913a4a9292b
Andrey-0367 <shestakov0367@yandex.ru>
2025-05-19 22:23:33 +0300

    fix: update deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/Geology',
-  
   output: 'export',
-  
-  distDir: 'out',
-  
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  
-  trailingSlash: true,
   
   webpack: (config) => {
     config.resolve.alias = {
