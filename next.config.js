85aea941990547ddf7eaadb6436c71e338a6ad7e
Andrey-0367 <shestakov0367@yandex.ru>
2025-05-19 19:09:29 +0300

    add

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+    webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'three/examples/jsm/controls/OrbitControls': 'three/examples/jsm/controls/OrbitControls.js',
+      'three/examples/jsm/loaders/GLTFLoader': 'three/examples/jsm/loaders/GLTFLoader.js'
+    };
+    return config;
+  }
 }
 
 module.exports = nextConfig
