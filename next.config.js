858eb44d3104e9a9bba169f799bcd0e5c933e64a
Andrey-0367 <shestakov0367@yandex.ru>
2025-05-20 17:46:02 +0300

    team

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
-  output: 'standalone',
+  output: 'export',
   basePath: '/Geology',
   trailingSlash: true,
   images: {
