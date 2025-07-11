b7f74602b7e4211b88e698ad28407ccdc4e9684c
Andrey-0367 <Andrey-0367@users.noreply.github.com>
2025-07-11 17:01:52 +0000

    deploy: 9faa772ebfcf702e666f9a860c0f7ffbd443b3a1

---
+++ _next/static/6gun5R9O_y58M8AmYgeKL/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fsale\u002F[slug]","\u002Fshop\u002Fcategory\u002F[categoryId]","\u002Fshop\u002Fcategory\u002F[categoryId]\u002Fproduct\u002F[productId]","\u002Fteam\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
