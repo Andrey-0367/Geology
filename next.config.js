af2c1d80ef78d3658a9b63ac8c6f251d29990fa8
Andrey-0367 <shestakov0367@yandex.ru>
2025-07-11 16:21:20 +0300

    test-2

--- next.config.js
+++ next.config.js
@@ -1,26 +1,27 @@
 /** @type {import('next').NextConfig} */
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   trailingSlash: true,
-  
+  output: "export",
+  trailingSlash: true,
+
   images: {
-    domains: [
-      'api.geologiya-ru.ru',
-      'geologiya-ru.ru',
-      'www.geologiya-ru.ru'
-    ],
+    unoptimized: true,
+    domains: ["api.geologiya-ru.ru", "geologiya-ru.ru", "www.geologiya-ru.ru"],
   },
 
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      'three/examples/jsm/controls/OrbitControls': 'three/examples/jsm/controls/OrbitControls.js',
-      'three/examples/jsm/loaders/GLTFLoader': 'three/examples/jsm/loaders/GLTFLoader.js'
+      "three/examples/jsm/controls/OrbitControls":
+        "three/examples/jsm/controls/OrbitControls.js",
+      "three/examples/jsm/loaders/GLTFLoader":
+        "three/examples/jsm/loaders/GLTFLoader.js",
     };
     return config;
-  }
-}
+  },
+};
 
 module.exports = nextConfig;
--- src/app/(pages)/shop/category/[categoryId]/product/[productId]/page.tsx
+++ src/app/(pages)/shop/category/[categoryId]/product/[productId]/page.tsx
@@ -1,6 +1,28 @@
-import { getProductById } from '@/api/products';
+import { fetchCategories } from '@/api/categories';
+import { getCategoryProducts, getProductById } from '@/api/products';
 import ProductDetail from '@/app/ui/components/ProductDetail/ProductDetail';
 import { notFound } from 'next/navigation';
+
+export async function generateStaticParams() {
+  const categories = await fetchCategories();
+  const allParams = [];
+  
+  for (const category of categories) {
+    const categoryId = String(category.id);
+    const products = await getCategoryProducts(categoryId);
+    
+    for (const product of products) {
+      const productId = String(product.id);
+      
+      allParams.push({
+        categoryId, 
+        productId,   
+      });
+    }
+  }
+  
+  return allParams;
+}
 
 export default async function ProductPage({
   params
